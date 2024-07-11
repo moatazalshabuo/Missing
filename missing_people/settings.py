de67d6614d3378d178f3ddcda4113c35aeaba2ea
Muetaz Alsahbu <muetaz.alsahbu@zallaf.com>
2024-07-11 16:14:48 +0200

    all edit

--- missing_people/settings.py
+++ missing_people/settings.py
@@ -78,9 +78,17 @@ WSGI_APPLICATION = 'missing_people.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'desertte_gold_store',
+        'HOST': 'localhost',
+        'USER': 'desertte_desertt',
+        'PASSWORD': 'V+d:50uDok5AL7'
+
     }
 }
 
