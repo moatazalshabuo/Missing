94b77926116096b7bb8587cd95a50639a1640f81
Muetaz Alsahbu <muetaz.alsahbu@zallaf.com>
2024-07-11 19:09:09 +0200

    all edit

--- missing_people/settings.py
+++ missing_people/settings.py
@@ -127,10 +127,10 @@ USE_TZ = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
-EMAIL_HOST = 'missing.desert-technology.com.ly'  # e.g., 'smtp.example.com'
+EMAIL_HOST = ''  # e.g., 'smtp.example.com'
 EMAIL_PORT = 465
-EMAIL_HOST_USER = 'info@missing.desert-technology.com.ly'  # Your cPanel email address
-EMAIL_HOST_PASSWORD = 'Cll8l927@123'
+EMAIL_HOST_USER = ''  # Your cPanel email address
+EMAIL_HOST_PASSWORD = ''
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
